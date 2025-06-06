7ee6a40a6769a43dc625c27ea5a927d1e1c83160
kaniikaaaa <157051411+kaniikaaaa@users.noreply.github.com>
2025-06-06 23:58:50 +0530

    index.js

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const langflowRoutes = require('./routes/langflow');
 
 app.use(express.json());
-
-// Import langflow routes
-const langflowRoutes = require('./routes/langflow');
 app.use('/api/langflow', langflowRoutes);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+const PORT = 3000;
+app.listen(PORT, () => console.log(`API server on http://localhost:${PORT}`));
